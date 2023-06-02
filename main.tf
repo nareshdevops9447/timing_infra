293c8f6ed254ce4bf057221538909641c0a718ec
nareshdocker9447 <nk122121@gmail.com>
2023-06-02 15:54:36 +0100

    updated main.tf

--- main.tf
+++ main.tf
@@ -2,4 +2,7 @@ module "vpc" {
     source = "../terraform_modules/vpc"
     vpc_cidr = var.vpc_cidr
     tags = var.tags
+    public_subnet_cidr = var.public_subnet_cidr
+    azs = var.azs
+    public_subnet_names = var.public_subnet_names
 }
--- variables.tf
+++ variables.tf
@@ -8,5 +8,21 @@ variable "tags" {
 }
 
 variable "vpc_cidr" {
+    list = string
   default = "10.0.0.0/16"
+}
+
+variable "azs" {
+    type = list
+    default = ["us-east-1a" , "us-east-1b"]
+}
+
+variable "public_subnet_cidr" {
+    type = list
+    default =["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "public_subnet_names" {
+    type = list
+    default = ["timing-public-1a","timing-public-1b"]
 }
