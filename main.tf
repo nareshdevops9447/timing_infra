d2299b663f5055dcb408d81f353967a5fc742a35
nareshdocker9447 <nk122121@gmail.com>
2023-06-02 18:28:34 +0100

    updated main.tf

--- main.tf
+++ main.tf
@@ -7,6 +7,6 @@ module "vpc" {
     public_subnet_names = var.public_subnet_names
     public_route_table_name = local.public_route_table_name
     private_subnet_cidr = var.private_subnet_cidr
-    private_subnet_name = var.private_subnet_name
+    private_subnet_names = var.private_subnet_names
     private_route_table_name = local.private_route_table_name
 }
