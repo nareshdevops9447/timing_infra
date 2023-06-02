f21e4d1f17a61cca58b49dfa4386368630694365
nareshdocker9447 <nk122121@gmail.com>
2023-06-02 18:23:00 +0100

    updated main.tf

--- variables.tf
+++ variables.tf
@@ -22,8 +22,17 @@ variable "public_subnet_cidr" {
     default =["10.0.1.0/24", "10.0.2.0/24"]
 }
 
-variable "public_subnet_names" {
+variable "public_subnet_name" {
     type = list
     default = ["timing-public-1a","timing-public-1b"]
 }
 
+variable "private_subnet_cidr" {
+    type = list
+    default =["10.1.0.0/24", "10.2.0.0/24"]
+}
+
+variable "private_subnet_name" {
+    type = list
+    default = ["timing-private-1a","timing-private-1b"]
+}
