0c1af2613af38edd4356af2920c08e5fba9955ab
nareshdocker9447 <nk122121@gmail.com>
2023-06-02 19:47:56 +0100

    updated

--- main.tf
+++ main.tf
@@ -6,9 +6,11 @@ module "vpc" {
     azs = var.azs
     public_subnet_names = var.public_subnet_names
     public_route_table_name = local.public_route_table_name
+    # private
     private_subnet_cidr = var.private_subnet_cidr
     private_subnet_names = var.private_subnet_names
     private_route_table_name = local.private_route_table_name
+    # public
     database_subnet_cidr = var.database_subnet_cidr
     database_subnet_names = var.database_subnet_names
     database_route_table_name = local.database_route_table_name
