600798ca0c1b4dbb2b3811c7f9a499fac78c36f6
nareshdocker9447 <nk122121@gmail.com>
2023-06-02 17:15:32 +0100

    updated main.tf

--- locals.tf
+++ locals.tf
@@ -1,4 +1,4 @@
 locals {
     # timing public this is equal to project name - public
-    public_route_table_names = format("%s-%s" , lookup(var.tags, "Names"), "public" )
+    public_route_table_name = format("%s-%s" , lookup(var.tags, "Names"), "public" )
 }
