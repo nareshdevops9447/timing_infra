c62b708aa6b9a394753faf068405a881cfb9dccf
nareshdocker9447 <nk122121@gmail.com>
2023-06-05 16:08:41 +0100

    updated sg.tf

--- rds.tf
+++ rds.tf
@@ -10,5 +10,6 @@ module "rds" {
     db_subnet_group_name = local.db_subnet_group_name
     vpc_security_group_ids = [local.rds_security_group_id]
     rds_secret_arn = var.rds_secret_arn
+    secret_name=var.secret_name
     tags = var.tags
 }
